86b471c6bf3b2e9983c9ce4f372434a332f2670d
Favour <nyamburafavour765@gmail.com>
2024-09-19 17:56:25 +0300

    Initial commit

---
+++ src/ABI.js
@@ -0,0 +1,34 @@
+const ABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_newMessage",
+				"type": "string"
+			}
+		],
+		"name": "setMessage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "getMessage",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+export default ABI;
